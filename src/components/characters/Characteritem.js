9626667ccfb4bd2c7106fcf8ad7987dc358b2830
priya <68297542+coder-eng@users.noreply.github.com>
2021-08-14 12:13:40 +0530

    Add files via upload

---
+++ src/components/characters/Characteritem.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Characteritem = ({ item }) => {
+  return (
+    <div className='card'>
+      <div className='card-inner'>
+        <div className='card-front'>
+          <img src={item.img} alt='' />
+        </div>
+        <div className='card-back'>
+          <h1>{item.name}</h1>
+          <ul>
+            <li>
+              <strong>Actor Name:</strong> {item.portrayed}
+            </li>
+            <li>
+              <strong>Nickname:</strong> {item.nickname}
+            </li>
+            <li>
+              <strong>Birthday:</strong> {item.birthday}
+            </li>
+            <li>
+              <strong>Status:</strong> {item.status}
+            </li>
+            <li>
+                <strong>Occupation:</strong> {item.occupation}
+            </li>
+            <li>
+                <strong>Seasons:</strong> {item.appearance}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Characteritem
